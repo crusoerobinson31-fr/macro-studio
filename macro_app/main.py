d055ee67cd2c61ca59f758302ee2441ea4d82e0f
crusoerobinson31-fr <crusoerobinson31@gmail.com>
2026-04-08 14:45:57 +0200

    Add main function to launch the application

--- macro_app/main.py
+++ macro_app/main.py
@@ -1,1 +1,12 @@
+from macro_app.ui import MainWindow
+from PySide6.QtWidgets import QApplication
+import sys
 
+def main():
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
